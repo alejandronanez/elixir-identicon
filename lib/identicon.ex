f39fb96f81c297d74809b5645a9f6a9f00220280
Alejandro Nanez <ananez@hugeinc.com>
2017-12-20 15:44:49 -0500

    Referencing mirror_row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,13 +9,12 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex} = image) do
     hex
     |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
   end
 
   def mirror_row(row) do
-    # [145, 46, 200]
     [first, second | _tail] = row
 
-    # [145, 46, 200, 46, 145]
     row ++ [second, first]
   end
 
